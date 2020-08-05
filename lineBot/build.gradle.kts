33d6881327ffd44646f6fa25801030bde7583a97
zipe <zipe.daden@gmail.com>
2020-08-05 23:06:55 +0800

    Refactoring code.

--- lineBot/build.gradle.kts
+++ lineBot/build.gradle.kts
@@ -7,22 +7,7 @@ dependencies {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.linecorp.bot:line-bot-spring-boot:3.6.0")
-    implementation("com.google.guava:guava:29.0-jre")
-    implementation("org.projectlombok:lombok:1.18.12")
-    implementation("commons-codec:commons-codec:1.14")
-    implementation("com.google.code.gson:gson:2.8.6")
-    implementation("org.apache.commons:commons-collections4:4.4")
-    implementation("org.apache.commons:commons-lang3:3.10")
-    implementation("com.vladmihalcea:hibernate-types-52:2.9.12")
-    implementation("org.apache.commons:commons-pool2:2.8.0")
-    implementation("redis.clients:jedis:3.3.0")
     implementation("org.jsoup:jsoup:1.13.1")
     implementation("com.squareup.okhttp3:okhttp:4.8.0")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
