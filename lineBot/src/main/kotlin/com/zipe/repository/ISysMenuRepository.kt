e108888b560876575d4606d275611ad625a53709
zipe <zipe.daden@gmail.com>
2021-02-22 22:56:54 +0800

    1. Adjust system menu query method.

--- lineBot/src/main/kotlin/com/zipe/repository/ISysMenuRepository.kt
+++ lineBot/src/main/kotlin/com/zipe/repository/ISysMenuRepository.kt
@@ -2,11 +2,9 @@ package com.zipe.repository
 
 import com.zipe.entity.SysMenu
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ISysMenuRepository : JpaRepository<SysMenu, Long> {
-    @Query("SELECT menu FROM SysMenu menu WHERE menu.enabled = true ORDER BY menu.parentId ASC, menu.sequence ASC")
-    fun findEnabled(): List<SysMenu>
+    fun findByEnabledOrderByParentIdAscSequenceAsc(enabled: Boolean): List<SysMenu>
 }
